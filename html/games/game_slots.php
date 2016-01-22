010d416e87d8aed74b0e5c0a64a96e3ff03af7bf
Santiago Fernández <vzhurur@gmail.com>
2016-01-22 16:29:55 +0100

    Example of game slot file

---
+++ html/games/game_slots.php
@@ -0,0 +1,22 @@
+<?php
+
+// Nombre de la partida
+$game_name = "Babylon 5";
+
+// Datos de los slots:
+// ------ User ----------- PlayerID -- Race Name
+$datos[ "nobody"    ] = array( "0", "nobody" );
+$datos[ "vzhuru"    ] = array( "1", "Fascist" );
+$datos[ "vzhuru"    ] = array( "2", "Fascist" );
+$datos[ "vzhuru"    ] = array( "3", "Fascist" );
+$datos[ "vzhuru"    ] = array( "4", "Fascist" );
+$datos[ "vzhuru"    ] = array( "5", "Fascist" );
+$datos[ "vzhuru"    ] = array( "6", "Fascist" );
+$datos[ "vzhuru"    ] = array( "7", "Fascist" );
+$datos[ "vzhuru"    ] = array( "8", "Fascist" );
+$datos[ "vzhuru"    ] = array( "9", "Fascist" );
+$datos[ "vzhuru"    ] = array( "10". "Fascist" );
+$datos[ "vzhuru"    ] = array( "11", "Fascist" );
+
+$upload_dir = "/home/vgap/"
+?>
