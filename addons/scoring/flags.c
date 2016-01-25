409c0fb08a40a2e11cc45e601fd38c44c5e21440
Santiago Fernández <vzhurur@gmail.com>
2016-01-25 18:47:42 +0100

    hidden flags, untested

--- addons/scoring/flags.c
+++ addons/scoring/flags.c
@@ -5,14 +5,16 @@ Computes the flags owned by each player and produces two results files
 Flags are configurated by a file, each flag has separates minimum values
 for Cols, Mines, Factories or Defense or default values. Defaults values
 can be defined also. 
+It is possible to use Hidden Flags, that is, flags whose planet id is 
+unknown to players. Or remove the planet Id by inserting a label after 
+default or list of structures.
 Since flags only reads host data it could be executed outside turn hosting.
 */
 
-#include <phost_user/phostpdk/phostpdk.h>
+#include <phostpdk/phostpdk.h>
 #include <stdio.h>
 #include <string.h>
 
-#define CONFIG_FILE "flags.txt"
 void flags_addon();
 
 int main(int argc, char** argv)
@@ -39,19 +41,20 @@ int main(int argc, char** argv)
 }
 
 Boolean ShowFlags = True;
-Uns32 DefaultMinCol = 100;
-Uns16 DefaultMinFactories = 0, DefaultMinMine = 0, DefaultMinDefenses = 1;
+unsigned DefaultMinCol = 100;
+unsigned short DefaultMinFactories = 0, DefaultMinMine = 0, DefaultMinDefenses = 1;
 
 Boolean AssignOption(const char *lhs, char *rhs, const char *lInputLine)
 {
     if ( lhs != NULL && rhs != NULL )
     {
         char option[256], value[256];
-        if ( 1 == sscanf(lhs, "%255s", option) && 0 == strcmp(lhs, "ShowFlags") )
+        if ( 1 == sscanf(lhs, "%255s", option) && 0 == strcmp(option, "ShowFlags") )
         {
-            ShowFlags = 1 == sscanf(rhs, "%255s", value) && 0 == strcmp(rhs, "Yes");
+            ShowFlags = ((1 == sscanf(rhs, "%255s", value)) && (0 == strcmp(value, "Yes")));
+            printf("ShowFlags set to %s (%i)\n", value, ShowFlags);
         }
-        else if ( 1 == sscanf(lhs, "%255s", option) && 0 == strcmp(lhs, "DefaultStructures") )
+        else if ( 1 == sscanf(lhs, "%255s", option) && 0 == strcmp(option, "DefaultStructures") )
         {
             if ( 4 != sscanf(rhs, "%u,%hu,%hu,%hu",
                              &DefaultMinCol, &DefaultMinFactories,
@@ -59,6 +62,11 @@ Boolean AssignOption(const char *lhs, char *rhs, const char *lInputLine)
             {
                 Warning("Bad format to define Default Structures, should be 4 numbers separated by commas");
                 return False;
+            }
+            else
+            {
+                printf("Default Structures set to: %u,%hu,%hu,%hu\n", DefaultMinCol, DefaultMinFactories,
+                             DefaultMinMine, DefaultMinDefenses);
             }
         }
         else
@@ -74,28 +82,34 @@ Uns16 flags[RACE_NR+1] = { 0 };
 
 Boolean ComputeFlags(const char *lhs, char *rhs, const char *lInputLine)
 {
-    Uns16 aPlanet;
+    unsigned short aPlanet;
 
-    if ( lhs != NULL && rhs != NULL && 1 == sscanf(lhs, "%hu", &aPlanet) )
+    if ( lhs != NULL && rhs != NULL  )
     {
-        if ( IsPlanetExist( aPlanet ) )
+        if (  (1 == sscanf(lhs, "%hu", &aPlanet)) && IsPlanetExist( aPlanet ) )
         {
-            Uns32 MinCol = DefaultMinCol;
-            Uns16 MinFactories = DefaultMinFactories,
+            unsigned MinCol = DefaultMinCol;
+            unsigned short MinFactories = DefaultMinFactories,
                   MinMine = DefaultMinMine, MinDefenses = DefaultMinDefenses;
+            char showId[256] = "ShowId";
             char value[256];
             int goal;
 
-            if ( 1 == sscanf(rhs, "%255s", value) && 0 == strcmp(value, "Default") )
+            if ( 2 == sscanf(rhs, " %255[^,], %255[^\n]", value, showId) && 0 == strcmp(value, "Default") )
+            {
+            } 
+            else if ( 1 == sscanf(rhs, "%255s", value) && 0 == strcmp(value, "Default") )
+            {
+            } 
+            else if ( 5 == sscanf(rhs, "%u,%hu,%hu,%hu, %255[^\n]",&MinCol, &MinFactories, &MinMine, &MinDefenses, showId) )
             {
             }
-            else if ( 4 == sscanf(rhs, "%u,%hu,%hu,%hu",
-                             &MinCol, &MinFactories, &MinMine, &MinDefenses) )
+            else if ( 4 == sscanf(rhs, "%u,%hu,%hu,%hu",&MinCol, &MinFactories, &MinMine, &MinDefenses) )
             {
             }
             else
             {
-                Error("Unknown target structures for flag, should be either 'Default' or 4 numbers");
+                Error("Unknown target structures for flag, should be either 'Default' or 4 numbers, optionally followed by a label to hide planet id");
                 return False;
             }
 
@@ -106,13 +120,26 @@ Boolean ComputeFlags(const char *lhs, char *rhs, const char *lInputLine)
 
             flags[PlanetOwner(aPlanet)] += goal;
 
-            if ( goal && ShowFlags == True )
+            if ( ShowFlags == True )
             {
-                printf("%10u %10u %10u %10u %10u %10u\n", aPlanet, PlanetOwner(aPlanet),
-                   PlanetCargo(aPlanet, COLONISTS),
-                   PlanetFactories(aPlanet),
-                   PlanetMines(aPlanet),
-                   PlanetDefense(aPlanet));
+                if ( 0 == strcmp(showId, "ShowId") ) 
+                {
+                    sprintf(showId, "%10u", aPlanet);
+                }
+                
+                if ( goal )
+                {
+                    printf("%s %10u \n", showId, PlanetOwner(aPlanet));
+                    /*printf("%10u %10u %10u %10u %10u %10u\n", aPlanet, PlanetOwner(aPlanet),
+                       PlanetCargo(aPlanet, COLONISTS),
+                       PlanetFactories(aPlanet),
+                       PlanetMines(aPlanet),
+                       PlanetDefense(aPlanet));*/
+                }
+                else
+                {
+                    printf("%s %10s\n", showId, PlanetOwner(aPlanet) == 0 ? "Unowned" : "Not enough");
+                }
             }
         }
         else
@@ -126,7 +153,7 @@ Boolean ComputeFlags(const char *lhs, char *rhs, const char *lInputLine)
 
 void flags_addon()
 {
-    const char* config_file_name = CONFIG_FILE;
+    const char* config_file_name = "flags.txt";
     int lPlayer;
 
     FILE *f = OpenInputFile(config_file_name, GAME_DIR_ONLY | TEXT_MODE);
@@ -141,15 +168,34 @@ void flags_addon()
             printf("\nFlag Report\n"
                    "==============\n"
                    "\n"
-                   "%10s %10s %10s %10s %10s\n"
-                   " ---------- ---------- ---------- ---------- ---------- ----------\n",
-                   "Planet", "Player", "Colonist" "Factories", "Mines", "Defenses");
+                   " %10s %10s\n " // "%10s %10s %10s %10s\n"
+                   " ---------- ---------- \n", // " ---------- ---------- ---------- ----------\n",
+                   "Planet", "Player" /*, "Colonist", "Factories", "Mines", "Defenses"*/);
         }
 
         f = OpenInputFile(config_file_name, GAME_DIR_ONLY | TEXT_MODE);
         ConfigFileReaderEx( f, config_file_name, "Flags", False, ComputeFlags, Error, False );
 
         printf("\nFlags Report by Player\n"
+               "========================\n"
+               "\n"
+               " Player    Flags\n"
+               "--------  ------- \n");
+        for ( lPlayer = 1; lPlayer <= RACE_NR; ++lPlayer)
+        {
+            if (PlayerIsActive(lPlayer))
+            {
+                printf("%4d      %4d\n", lPlayer, flags[lPlayer]);
+            }
+        }
+
+        fclose(f);
+
+        ShowFlags = False;
+        f = OpenInputFile(config_file_name, GAME_DIR_ONLY | TEXT_MODE);
+        ConfigFileReaderEx( f, config_file_name, "HIDDEN_FLAGS", False, ComputeFlags, Error, False );
+
+        printf("\nHidden Flags Report by Player\n"
                "========================\n"
                "\n"
                " Player    Flags\n"
